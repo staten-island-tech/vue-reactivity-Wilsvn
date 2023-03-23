46191e5b0222b6e4fda847c5e4e7120e919497e5
Wilsvn <wilsonvposcablo@gmail.com>
2023-03-23 10:13:31 -0400

    w

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,6 +1,16 @@
-<script setup></script>
+<script setup>
+import PizzaMaker from "./HeaderSection.vue";
+export default{
+  name:""
+components:{PizzaMaker}
+}
+</script>
 
 <template>
+  <h1>{{}}</h1>
+  <nav>
+    <RouterLink to="/header"> </RouterLink>
+  </nav>
   <main></main>
 </template>
 
---
+++ vue-project/src/views/HeaderSection.vue
@@ -0,0 +1,9 @@
+<template></template>
+
+<script>
+export default {
+  name: "PizzaMaker",
+};
+</script>
+
+<style lang="scss" scoped></style>
